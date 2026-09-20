6059ad8a9a0dde3c1ae19c7d7a03fe70821869ac
mariellem1994 <marielle_mos_1994@hotmail.com>
2026-09-20 14:47:39 +0200

    Update URLs for Lobbes products in bot.py

--- bot.py
+++ bot.py
@@ -293,43 +293,43 @@ products = [
 lobbes_products = [
     {
         "name": "Niceberg",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-niceberg-needoh"
     },
     {
         "name": "Gumdrop",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-gumdrop-needoh"
     },
     {
         "name": "Cool Cats",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-cool-cats-kat"
     },
     {
         "name": "Color Change",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-color-change"
     },
     {
         "name": "Nice Cube Glow",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-nice-cube-glow-needoh"
     },
     {
         "name": "Nice Cube",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-nice-cube-sensorisch-stressspeeltje-met-goo-vulling"
     },
     {
         "name": "Teenie Glob",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-teenie-glob-kleur-3-pack"
     },
     {
         "name": "Dream Pop",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-dream-pop-needoh"
     },
     {
         "name": "Wonder Waves",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-fuzz-ball-wonder-waves-needoh"
     },
     {
         "name": "Mello Mallo",
-        "url": "https://www.lobbes.nl/merken/needoh"
+        "url": "https://www.lobbes.nl/product/needoh-mello-mallo-needoh"
     }
 ]
 
